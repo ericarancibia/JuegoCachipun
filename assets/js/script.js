84146b3e39c03dfaf434fb134a49ad512441612a
ericarancibia <es.arancibia@gmail.com>
2024-03-06 22:22:52 -0300

    Se agrega contador de jugadas ganadas de PC y usuario, mostrando resultado global en documento al completar nro de jugadas solicitadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-let num_jugadas = Number(prompt("Indique cuantas veces quiere jugar contra el PC:"));
+let num_jugadas = Number(prompt("Indique cuántas veces quiere jugar contra la PC:"));
 let hist_resultados = "";
+let contador_usuario = 0;
+let contador_pc = 0;
 
 function turno_pc() {
     let num_random = Math.floor(Math.random() * 3);
@@ -17,7 +19,7 @@ function turno_pc() {
 for (let i = 0; i < num_jugadas; i++) {
     let pc = turno_pc();
     let respuesta = prompt("Ingrese: Piedra, Papel o Tijera").toLowerCase();
-    if (respuesta === "piedra" || respuesta === "papel" || respuesta === "tijera"){
+    if (respuesta === "piedra" || respuesta === "papel" || respuesta === "tijera") {
         let resultado = "";
 
         switch (true) {
@@ -28,20 +30,30 @@ for (let i = 0; i < num_jugadas; i++) {
             case respuesta === "papel" && pc === "piedra":
             case respuesta === "tijera" && pc === "papel":
                 resultado = "¡Partida Ganada!";
+                contador_usuario++;
                 break;
             default:
                 resultado = "Partida Perdida...";
+                contador_pc++;
                 break;
         }
 
         alert("Su jugada: " + respuesta + "\nJugada del PC: " + pc + "\nResultado: " + resultado);
         hist_resultados = hist_resultados + resultado + "<br>";
-    } 
-
-    else {
+    } else {
         alert("Palabra no reconocida. Por favor, intente nuevamente recargando la página.");
     }
 }
 
+if (contador_usuario > contador_pc) {
+    document.getElementById("resultado_global").innerHTML = "🎉🎉 ¡Usted! 🎉🎉";
+} else if (contador_usuario < contador_pc) {
+    document.getElementById("resultado-global").innerHTML = "😢 El PC 😢 ¡Sigue intentándolo!";
+} else {
+    document.getElementById("resultado_global").innerHTML = "🤝 ¡Empate! 🤝";
+}
+
+
 document.getElementById("num_jugadas").innerHTML = num_jugadas;
 document.getElementById("hist_resultados").innerHTML = hist_resultados;
+
